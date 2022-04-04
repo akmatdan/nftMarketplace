720819254cf678f05bf2fbae976324095778e39a
Dan Akmat <akmatdan@gmail.com>
2022-04-05 03:13:49 +0600

    Updated SubInfo

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -3,6 +3,7 @@ import { useNavigation } from '@react-navigation/native'
 
 import { COLORS, SHADOWS, SIZES, assets } from '../constants'
 import { CircleButton, RectButton } from './Button'
+import { SubInfo, NFTTitle, EthPrice } from './SubInfo'
 
 const NFTCard = ({ data }) => {
   const navigation = useNavigation()
@@ -15,6 +16,8 @@ const NFTCard = ({ data }) => {
       margin: SIZES.base,
       ...SHADOWS.dark
     }}>
+
+    {/* heart button */}
       <View style={{
         width: "100%",
         height: 250
@@ -29,8 +32,32 @@ const NFTCard = ({ data }) => {
             borderTopRightRadius: SIZES.font,
           }}
         />
-
         <CircleButton imgUrl={assets.heart} right={10} top={10} />
+      </View>
+
+      <SubInfo />
+
+      <View style={{ width: "100%", padding: SIZES.font }}>
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+
+        <View style={{ 
+          marginTop: SIZES.font,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center"
+        }}>
+          <EthPrice price={data.price}/>
+          <RectButton 
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
+        </View>
       </View>
     </View>
   )
